8176921f6f068e011edaba821ef6e7193beeffe4
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-21 23:12:39 +0530

    Signup Details

---
+++ JavaScript/ourSignupForm.js
@@ -0,0 +1,17 @@
+const uid = "12345abcd";
+var firstName = "Apurv";
+var lastName = "Gupta";
+var isLoggedIn = false;
+var country = "india";
+var email = "abc@abc.com";
+var password = 123456;
+var confirmPassword = 123456;
+var number = 123456789;
+
+console.log(`My FirstName is: ${firstName}
+and LastName is: ${lastName}
+and this user is not logged: ${isLoggedIn}
+and i am from: ${country}
+my email is: ${email}
+password is: ${password}
+number is: ${number}`);
---
+++ JavaScript/variables.js
@@ -0,0 +1,7 @@
+var fullName = "Apurv Gupta";
+var isLoggedIn = true;
+var loggedCount = 34;
+
+var paymentMode;
+paymentMode = "Credit Card";
+console.log(paymentMode);
