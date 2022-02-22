fdbc84585a70fac780d75a3e63f245ec836239c2
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-22 14:42:54 +0530

    Switch Case

---
+++ SwitchCases.js
@@ -0,0 +1,25 @@
+// Create an application with following rules
+//admin - get full access
+//subadmin - gets access to create/ delete courses
+//testprep - gets access to create/ delete tests
+//user - gets access to consume content
+
+var user = "admin";
+
+switch (user) {
+  case "admin":
+    console.log("Admin here");
+    break;
+  case "subadmin":
+    console.log("subadmin here");
+    break;
+  case "testprep":
+    console.log("testprep here");
+    break;
+  case "user":
+    console.log("user here");
+    break;
+  default:
+    console.log("trial user");
+    break;
+}
