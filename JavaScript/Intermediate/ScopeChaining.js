7417836e22b23f41b5578ed217d3deff90f0318b
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-22 22:05:57 +0530

    Scope Chaining

---
+++ JavaScript/Intermediate/ScopeChaining.js
@@ -0,0 +1,17 @@
+//Always ask help who is bigger then you
+
+var name = "Apurv";
+console.log(name);
+
+function sayName() {
+  var name = "Mr. Apurv";
+  console.log(name);
+
+  function sayMe() {
+    // var name = "Mr. Appu";
+    console.log(name);
+  }
+  sayMe();
+}
+
+sayName();
