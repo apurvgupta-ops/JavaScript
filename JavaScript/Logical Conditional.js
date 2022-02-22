d5246634fe5b736ec47b991770d3ac20daef8f86
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-22 13:44:33 +0530

    Logical Conditional

---
+++ JavaScript/Logical Conditional.js
@@ -0,0 +1,21 @@
+var isLoggedIn = true;
+var emailVarified = false;
+var cardInfo = true;
+
+// First Way
+// if (isLoggedIn) {
+//   console.log("You are logged in");
+//   if (emailVarified) {
+//     console.log("email varified done");
+//   }
+//   if (cardInfo) {
+//     console.log("you can purchase now");
+//   }
+// }
+
+// Second way
+if (isLoggedIn && emailVarified && cardInfo) {
+  console.log("You can buy now");
+} else {
+  console.log("You are not authenticate");
+}
