38412beca6336ed07b095fba0c55f0a4565fb3f0
apurvgupta-ops <56751664+apurvgupta-ops@users.noreply.github.com>
2022-02-23 13:29:27 +0530

    Array basic

--- JavaScript/Intermediate/Array.js
+++ JavaScript/Intermediate/Array.js
@@ -1,5 +1,5 @@
 var user = ["Apurv", "abc@abc.com", 2, 34];
-console.log(user); //Print full Array
+// console.log(user); //Print full Array
 // console.log(user[0]); //print the index value
 // console.log(user.pop()); //it remove the last value
 // console.log(user.unshift("appu")); //unshift is use to add the new value in the start
@@ -7,4 +7,11 @@ console.log(user); //Print full Array
 // console.log(user.slice(0, 2));
 // console.log(user); //Print full Array
 // console.log(Array.from("apurv")); // print the output in the form of array
-console.log(user.indexOf(34)); // it gives the index of the value
+// console.log(user.indexOf(34)); // it gives the index of the value
+
+const number = [2, 3, 4, 5, 6, 7, 8, 9];
+// const result = number.fill("A", 2, 6);
+// console.log(result);
+
+const result2 = number.filter((num) => num > 5);
+console.log(result2);
